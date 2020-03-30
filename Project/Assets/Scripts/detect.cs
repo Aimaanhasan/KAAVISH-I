986c5604e1e53ea27261cd55ec30e6e29e318089
= <=>
2020-03-30 20:12:25 +0500

    test

--- Project/Assets/Scripts/detect.cs
+++ Project/Assets/Scripts/detect.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//test
 public class detect : MonoBehaviour
 {
     // Start is called before the first frame update
